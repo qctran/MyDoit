781abd70ca55ca8bb621d0552371292d98a71db9
Conan Tran <conantran@gmail.com>
2018-01-24 21:13:44 -0800

    Add tableView with makeTasks()

---
+++ MyDoit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  MyDoit
+//
+//  Created by Conan Tran on 1/24/18.
+//  Copyright © 2018 Conan Tran. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- MyDoit/ViewController.swift
+++ MyDoit/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// step 2
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // step 5
+        tasks = makeTasks()
+        
+        // step 1
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // step 3
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    // step 4
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        // step 6
+        let task = tasks[indexPath.row]
+        
+        if task.important {
+            cell.textLabel?.text = "!\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
     }
 
-
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the cat"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy dinner"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Go see movie"
+        task3.important = false
+        
+        return [task1, task2, task3]
+    }
 }
 
